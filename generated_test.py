c926db8259d360a6c4cfbdad4c8c0209ad659cef
Subtilizer0102 <vmnandurkar@gmail.com>
2025-07-31 14:37:27 +0530

    generator.py added

--- generated_test.py
+++ generated_test.py
@@ -2,26 +2,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
-# Initialize the driver
 driver = webdriver.Chrome(service=Service())
-driver.get("https://www.saucedemo.com")
+driver.get("https://www.amazon.in/ap/register?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_ya_signin&prevRID=0FFRJ6D8EV6SJRKN3TBW&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
 
-# Wait for the page to load and elements to be present
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "user-name")))
+wait = WebDriverWait(driver, 10)
 
-# Fill in username and password
-driver.find_element(By.ID, "user-name").send_keys("test_user")
-driver.find_element(By.ID, "password").send_keys("test_password")
+name_input = wait.until(EC.presence_of_element_located((By.ID, "ap_customer_name")))
+name_input.send_keys("Test User")
 
-# Click the login button
-driver.find_element(By.ID, "login-button").click()
+phone_input = wait.until(EC.presence_of_element_located((By.ID, "ap_phone_number")))
+phone_input.send_keys("9876543210")
 
-# Wait for the next page to load or perform further actions
-WebDriverWait(driver, 10).until(EC.url_to_be("https://www.saucedemo.com/inventory.html"))
+password_input = wait.until(EC.presence_of_element_located((By.ID, "ap_password")))
+password_input.send_keys("Test@1234")
 
+continue_button = wait.until(EC.element_to_be_clickable((By.ID, "continue")))
+continue_button.click()
+
+time.sleep(2)
 time.sleep(5)
 driver.quit()
